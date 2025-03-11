c1da3f0cf36430b88149e8b2981195f6f769baaf
XX12 <ahrhshdndnfjf@gmail.com>
2025-03-11 23:35:31 +0800

    Initial Commit

---
+++ BasicMod-master/src/main/java/dbdmod/cards/skill/Defend.java
@@ -0,0 +1,41 @@
+package dbdmod.cards.skill;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import dbdmod.cards.BaseCard;
+import dbdmod.character.MyCharacter;
+import dbdmod.util.CardStats;
+
+public class Defend extends BaseCard {
+    public static final String ID = makeID(Defend.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.BASIC,
+            CardTarget.SELF,
+            1
+    );
+
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 3;
+
+    public Defend() {
+        super(ID, info);
+
+        setBlock(BLOCK, UPG_BLOCK);
+
+        tags.add(CardTags.STARTER_DEFEND);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, BLOCK));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Defend();
+    }
+}
