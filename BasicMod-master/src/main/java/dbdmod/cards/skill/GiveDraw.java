ac831707b3da4783b58d3206efa61aedf83c449e
XX12 <ahrhshdndnfjf@gmail.com>
2025-05-21 16:50:28 +0800

    新增: 懶得寫了 更新很多

---
+++ BasicMod-master/src/main/java/dbdmod/cards/skill/GiveDraw.java
@@ -0,0 +1,34 @@
+package dbdmod.cards.skill;
+
+import basemod.patches.com.megacrit.cardcrawl.screens.compendium.CardLibraryScreen.NoCompendium;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import dbdmod.cards.BaseCard;
+import dbdmod.character.MyCharacter;
+import dbdmod.util.CardStats;
+
+@NoCompendium
+public class GiveDraw extends BaseCard {
+    public static final String ID = makeID(GiveDraw.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.SPECIAL, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.NONE, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            -2 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    public GiveDraw() {
+        super(ID, info);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        onChoseThisOption();
+    }
+
+    public void onChoseThisOption() {
+        addToBot(new DrawCardAction(3));
+    }
+}
